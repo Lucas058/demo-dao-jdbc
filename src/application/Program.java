68ea7da6671a6e7e980e9069ec9a57640f2e26d7
Lucas058 <lucasbrandao.a@gmail.com>
2019-10-15 14:58:12 -0300

    Aula 244 - update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		sellerDao.insert(NewSeller);
 		System.out.println("Inserted! new id = " + NewSeller.getId());
 		
+		System.out.println("\n=========== Test 5: Seller update ============");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
